170dec81e33d9037a347a73a97dffec137e74bf4
Philip Henriksen <phe@pt-eng.no>
2019-06-17 21:56:26 +0200

    Removed incompatible code

--- BACnet/BACnetCodeSys2ExtLib/src/runner.cpp
+++ BACnet/BACnetCodeSys2ExtLib/src/runner.cpp
@@ -7,10 +7,18 @@ void runner(void)
 	cpp_runner();
 }
 
-// Actual C++ runner
+//// Defunct C++ runner
+//void cpp_runner(void)
+//{
+//	uint32_t* value = new uint32_t(16); // This valid C++ line seems to be causing problems
+//
+//	(void)value;
+//}
+
+// Working C++ runner
 void cpp_runner(void)
 {
-	uint32_t* value = new uint32_t(16); // This valid C++ line seems to be causing problems
+	uint32_t value = 16;
 
 	(void)value;
 }
