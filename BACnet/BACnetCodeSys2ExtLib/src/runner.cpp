2e4e434b1cbc622271ab40e830d69ee138580075
Philip Henriksen <phe@pt-eng.no>
2019-06-18 07:10:23 +0200

    Check if adding "delete" helps

--- BACnet/BACnetCodeSys2ExtLib/src/runner.cpp
+++ BACnet/BACnetCodeSys2ExtLib/src/runner.cpp
@@ -7,18 +7,20 @@ void runner(void)
 	cpp_runner();
 }
 
-//// Defunct C++ runner
+// Defunct C++ runner
+void cpp_runner(void)
+{
+	uint32_t* value = new uint32_t(16); // This valid C++ line seems to be causing problems
+
+	(void)value;
+
+	delete value;
+}
+
+//// Working C++ runner
 //void cpp_runner(void)
 //{
-//	uint32_t* value = new uint32_t(16); // This valid C++ line seems to be causing problems
+//	uint32_t value = 16;
 //
 //	(void)value;
 //}
-
-// Working C++ runner
-void cpp_runner(void)
-{
-	uint32_t value = 16;
-
-	(void)value;
-}
